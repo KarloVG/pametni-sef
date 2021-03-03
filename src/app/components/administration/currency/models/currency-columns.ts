59f455c9f8fd95e0c90fffdd44aecbb67ef19fa8
Karlo Bukovac <karlo.bukovac@fleksbit.hr>
2021-03-03 09:55:41 +0100

    Fixing currency table width, buttons

--- src/app/components/administration/currency/models/currency-columns.ts
+++ src/app/components/administration/currency/models/currency-columns.ts
@@ -3,15 +3,15 @@ import { TableColumn } from "@swimlane/ngx-datatable";
 export const CURRENCY_CRO: TableColumn[] = [
     { name: '#', prop: 'id', width: 50 },
     { name: 'Kod valute', prop: 'name', width: 300 },
-    { name: 'Omjer', prop: 'ratio', width: 500 },
-    { name: 'Apoenska struktura', prop: 'apoen', width: 100 },
+    { name: 'Omjer', prop: 'ratio', width: 250 },
+    { name: 'Akcije valuta', prop: 'apoen', width: 300 },
     { name: 'Akcije', prop: 'actions', width: 20 }
 ];
 
 export const CURRENCY_ENG: TableColumn[] = [
     { name: '#', prop: 'id', width: 50 },
     { name: 'Currency code', prop: 'name', width: 300 },
-    { name: 'Ratio', prop: 'ratio', width: 500 },
-    { name: 'Apoen structure', prop: 'apoen', width: 100 },
+    { name: 'Ratio', prop: 'ratio', width: 250 },
+    { name: 'Currency action', prop: 'apoen', width: 300 },
     { name: 'Actions', prop: 'actions', width: 20 }
 ];
